4768473039b9df4a0bd3a24ce1876e39bf83e9a3
kodam-nikhila <nikhilakodam259@gmail.com>
2022-05-08 22:59:07 +0530

    Graded assignment 2

---
+++ src/com/gl/javafsd/BTToSkewed/Node.java
@@ -0,0 +1,15 @@
+package com.gl.javafsd.BTToSkewed;
+import java.io.*;
+//class Node
+public class Node {
+ int number;
+ Node left,right;
+  Node(int value){
+	
+	 number= value;
+	 left=right=null;
+	 
+ }
+ 
+}
+
